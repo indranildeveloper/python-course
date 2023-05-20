f570bd4563a6cfe45e6d1bc3400b3f10284622c1
Indranil Halder <indranilhalder.dev@gmail.com>
2023-05-21 02:19:47 +0530

    Data Types

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello World!")
-print("Hi World!")
+# print("Hello World!")
+# print("Hi World!")
 
-print("Hi again!")
+# print("Hi again!")
+
+print(type("Hi"))
+print(type(2))
+print(type(2.5))
