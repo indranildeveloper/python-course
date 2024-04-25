0a6218a15d2e9127aefa3297af817b123876553a
Indranil Halder <indranilhalder.dev@gmail.com>
2024-04-25 21:34:12 +0530

    First program

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Remaru Tempest")
+
+# Python code -> Python Interpreter -> 
+# Convert to bytecode -> Runs the bytecode in the cpython VM -> 
+# Finally result is printed
+
+name = input("Write your name here: ")
+print("Hey there! " + name)
