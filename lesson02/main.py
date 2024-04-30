6d5113c4a33a43939fcb2c72371e2b5e6886843e
Indranil Halder <indranilhalder.dev@gmail.com>
2024-04-30 20:01:08 +0530

    Lesson01

---
+++ lesson02/main.py
@@ -0,0 +1,8 @@
+# Fundamentals of Programming
+
+"""
+a. Terms -> statements, variables etc.
+b. Data Types -> the types of data that a programming language has
+c. Actions -> store data -> get data -> perform some actions
+d. Best practices -> good and bad ways of writing code
+"""
