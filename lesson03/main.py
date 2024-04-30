cec5ebcab105ca057e8c6d8567f492430fa7981c
Indranil Halder <indranilhalder.dev@gmail.com>
2024-04-30 20:09:39 +0530

    Lesson03

---
+++ lesson03/main.py
@@ -0,0 +1,27 @@
+# Python Data Types
+"""
+-> Fundamental Data Types
+Numeric
+    int
+    float
+    complex
+Boolean
+    bool
+String
+    str
+Sequence
+    list
+    tuple
+    range
+Set
+    set
+    frozenset
+Mapping
+    dict
+
+Classes -> custom data types
+
+Modules -> Specialize data types
+
+None
+"""
