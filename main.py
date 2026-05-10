eace5edb7e87b7e5e54bec68bd840b60c4dfd079
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-10 21:48:28 +0530

    lesson 4 - basic arithmatic operators
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-print(40)
-print(5.67)
-print(3 + 5j)
+"""
+Python Arithmetic Operators
 
-print(type(40))
-print(type(5.67))
-print(type(3 + 5j))
+Arithmetic operators are used with numeric values to perform common mathematical operations:
 
-print(10 + 12)
-print(4 + 5.0)
++	Addition
+-	Subtraction
+*	Multiplication
+/	Division
+%	Modulus
+**	Exponentiation
+//	Floor division
+
+PEMDAS is an acronym used to remember the order of operations in mathematics: Parentheses, Exponents, Multiplication & Division (left-to-right), and Addition & Subtraction (left-to-right)
+"""
+
+print(3 + 2)
+print(5 + 6.0)
+print(10 - 6)
+print(5 * 6)
+print(5 * 6.0)
+print(4 / 5)
+print(10 / 5)
+
+print((5 + 6) / 2)
