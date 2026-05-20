8195f6b0953b17e6dc6890d7effbba5b935347a1
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-20 16:42:08 +0530

    string concatenation
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 """
-String Escape Characters
+Python - String Concatenation
 
-Escape Character
-To insert characters that are illegal in a string, use an escape character.
-
-An escape character is a backslash \\ followed by the character you want to insert.
-
-An example of an illegal character is a double quote inside a string that is surrounded by double quotes:
-
-\'	Single Quote
-\"	Double Quote
-\\	Backslash
-\n	New Line
-\r	Carriage Return
-\t	Tab
-\b	Backspace
-\f	Form Feed
+String Concatenation / Join Strings
+To concatenate, or combine, two strings you can use the + operator.
 """
 
-msg = "hello\tworld"
+str_one = "Hello"
+str_two = "World"
+msg = str_one + " " + str_two
+
 print(msg)
+
+ice_str = "ice "
+# cream_str = "cream"
+ice_str += "cream"
+
+print(ice_str)
+
+a = 20
+a -= 50
+print(a)
