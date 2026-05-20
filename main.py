1f6494e619b772a453a1eb966755ad9dc43a65b9
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-20 17:25:45 +0530

    type conversion
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 """
-Python String Index
-
-Like many other popular programming languages, strings in Python are arrays of unicode characters.
-
-Square brackets can be used to access elements of the string.
+Python Type Conversion
 """
 
-msg = "Hello World"
-print(len(msg))
+num_one = 10
+num_two = float(num_one)
+print(num_two)
+print(type(num_two))
+
+price = 10
+
+msg = "The price is " + str(price)
+print(msg)
