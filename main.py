2376960f974ea3245e7399e17b2e4c1e35e2ff0f
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-22 17:46:53 +0530

    multiple logical operator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -6,19 +6,9 @@ Combining Multiple Operators
 You can combine multiple logical operators in a single expression. Python evaluates not first, then and, then or.
 """
 
-<<<<<<< HEAD
 age = 25
 is_student = False
 has_discount_code = True
 
 if (age < 18 or age > 65) and not is_student or has_discount_code:
     print("Discount Applied!")
-=======
-a = 40
-b = 100
-
-if not a > b:
-    print("a is not grater than b.")
-else:
-    print("a is not less than b.")
->>>>>>> fbee6e81e1734642349481799f6f9940ff18abd7
