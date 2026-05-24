5ff402f25bead70b682a3f9201129242ebb9eb6d
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-24 17:25:54 +0530

    enumerate function
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 """
-Python range() Function
+Python enumerate() Function
 
-The range() function returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number.
+The enumerate() function takes a collection (e.g. a tuple, a string) and returns it as an enumerate object.
+
+The enumerate() function adds a counter as the key of the enumerate object.
 """
 
-nums = range(0, 101, 5)
-# nums = range(10, 0, -1)
-# print(list(nums))
+message = "hello"
+# print(list(enumerate(message)))
 
-for num in nums:
-    print(num)
+for index, character in enumerate(message):
+    print(f"The character at the index {index} is {character}")
