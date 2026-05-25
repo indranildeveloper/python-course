54645e993b49f228943cf38c876717a01e2bb3fd
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-25 20:02:38 +0530

    add items to a list
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 """
-Iterating over lists
+List Methods
+
+Append Items
+To add an item to the end of the list, use the append() method.
+
+Insert Items
+To insert a list item at a specified index, use the insert() method.
+The insert() method inserts an item at the specified index.
+
+Extend List
+To append elements from another list to the current list, use the extend() method.
 """
 
-fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+fruits = ["apple", "banana", "cherry", "orange"]
+# fruits.append("mango")
+# second_fruit_list = ["mango", "kiwi"]
+# fruits.extend(second_fruit_list)
 
-for fruit in fruits:
-    print(fruit)
+fruits.insert(2, "mango")
 
-for idx in range(len(fruits)):
-    print(fruits[idx])
-
-idx = 0
-
-while idx < len(fruits):
-    print(f"{idx}: {fruits[idx]}")
-    idx += 1
-
-for idx, fruit in enumerate(fruits):
-    print(f"{idx}: {fruit}")
+print(fruits)
