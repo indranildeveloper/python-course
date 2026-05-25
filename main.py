af1dae7ab9428c1c0d31f4c31ef57a28b40cef32
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-25 19:26:24 +0530

    access item in list
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 """
-Python Lists
+Python - Access List Items
 
-Lists are used to store multiple items in a single variable.
+List items are indexed and you can access them by referring to the index number.
 
-In other programming languages we have the similar data structure which is Array.
-
-Lists are ordered
-
-When we say that lists are ordered, it means that the items have a defined order, and that order will not change.
-
-If you add new items to a list, the new items will be placed at the end of the list.
+The first item has index 0.
 """
 
-fruit_one = "apple"
-fruit_two = "orange"
-fruit_three = "banana"
+fruits_list = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits_list[0])
 
-fruits_list = [fruit_one, fruit_two, fruit_three]
-my_list = ["abc", True, True, False, 5, 6.4]
-
-print(len(fruits_list))
-print(type(my_list))
-
-r = range(10)
-print(list(r))
-
-fruits_list = list(("apple", "banana", "cherry"))
-print(fruits_list)
+print("apple" in fruits_list)
