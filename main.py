56b305ebd607c78167fae064a7f6e624772a8b23
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-26 19:00:45 +0530

    list comprehension introduction
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 """
-Swapping list elements
+Python - List Comprehension
+
+List comprehension offers a shorter syntax when you want to create a new list based on the values of an existing list.
+
+The Syntax
+newlist = [expression for item in iterable if condition == True]
 """
 
-fruits = ["apple", "banana"]
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 
-fruits[0], fruits[1] = fruits[1], fruits[0]
+fruits_upper = []
+
+for fruit in fruits:
+    fruits_upper.append(fruit.upper())
+
+fruits_upper = [fruit.upper() for fruit in fruits]
 
 print(fruits)
+print(fruits_upper)
