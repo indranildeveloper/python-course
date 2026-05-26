fbdadcedc20ee0f4ced9e2e6e6c3662ca89d07b0
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-26 18:36:30 +0530

    list slicing
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 """
-List Methods
+List Slicing
 
-Python List index() Method
-The index() method returns the position at the first occurrence of the specified value.
+The general syntax for a slice is: list[start:stop:step]
 
-Python List count() Method
-The count() method returns the number of elements with the specified value.
-
-Python List reverse() Method
-The reverse() method reverses the sorting order of the elements.
-
-Python List sort() Method
-The sort() method sorts the list ascending by default.
-
-Python String join() Method
-The join() method takes all items in an iterable and joins them into one string.
+It creates a copy of the list.
 """
 
 fruits = ["apple", "banana", "cherry", "kiwi", "mango", "orange", "cherry"]
 
-print(fruits.index("apple"))
-print(fruits.count("cherry"))
+fruits_copy = fruits[6:1:-1]
 
-fruits.reverse()
-fruits.sort(reverse=False)
-print(fruits)
-
-fruits_str = "#".join(fruits)
-print(fruits_str)
+print(fruits_copy)
