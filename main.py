ebda7fb1807f2ceaf698bf9a3d0ae92e61e5e745
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-27 13:11:15 +0530

    python dictionary
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 """
-Nested List
+Python Dictionaries
+
+Dictionary
+
+Dictionaries are used to store data values in key:value pairs.
+A dictionary is a collection which is ordered, changeable and do not allow duplicates.
+
+The keys can be numbers or strings and the values can be any data type.
+
+Ordered or Unordered?
+
+As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+When we say that dictionaries are ordered, it means that the items have a defined order, and that order will not change.
+
+Unordered means that the items do not have a defined order, you cannot refer to an item by using an index.
+
+Changeable
+
+Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
 """
 
-# for list in nested_list:
-#     for num in list:
-#         print(num)
+car_details = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "is_active": False,
+    "colors": ["red", "black", "purple"],
+}
 
-# nested_list = [[j for j in range(1, 4)] for i in range(1, 4)]
+person_details = dict(name="John", age=36, country="USA")
 
-nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-flat_list = [item for row in nested_list for item in row]
-
-print(nested_list)
-print(flat_list)
+print(person_details)
