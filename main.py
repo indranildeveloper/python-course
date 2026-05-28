d3bf361fe7cec32ac5eaa173ce616ddd0867224e
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-28 18:43:37 +0530

    clear copy fromkeys and get methods
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 """
 Python Dictionaries
+
+Python Dictionary clear() Method
+The clear() method removes all the elements from a dictionary.
+
+Python Dictionary copy() Method
+The copy() method returns a copy of the specified dictionary.
+
+Python Dictionary fromkeys() Method
+The fromkeys() method returns a dictionary with the specified keys and the specified value.
+
+Python Dictionary get() Method
+The get() method returns the value of the item with the specified key.
 """
 
 car_details = {
@@ -10,8 +22,16 @@ car_details = {
     "colors": ["red", "black", "purple"],
 }
 
-print("abc" in car_details.keys())
-print("Ford" in car_details.values())
+print(car_details["brand"])
+print(car_details.get("abc"))
 
-if "model" in car_details:
-    print("Yes, 'model' is one of the keys.")
+# car_details.clear()
+
+# car_details_copy = car_details.copy()
+# print(car_details is car_details_copy)
+
+# x = ["key1", "key2", "key3"]
+# y = 0
+
+# demo_dictionary = dict.fromkeys(x, y)
+# print(demo_dictionary)
