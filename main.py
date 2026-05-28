d8a18f6bfe580d708daf593d37d688f3915e6f14
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-28 18:23:00 +0530

    in keyword with dictionary
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 """
 Python Dictionaries
-
-Accessing Items
-
-You can access the items of a dictionary by referring to its key name, inside square brackets.
-There is also a method called get() that will give you the same result.
-
-Get Keys
-The keys() method will return a list of all the keys in the dictionary.
-
-Get Values
-The values() method will return a list of all the values in the dictionary.
-
-Get Items
-The items() method will return each item in a dictionary, as tuples in a list.
 """
 
 car_details = {
@@ -24,16 +10,8 @@ car_details = {
     "colors": ["red", "black", "purple"],
 }
 
-for x in car_details:
-    print(car_details[x])
+print("abc" in car_details.keys())
+print("Ford" in car_details.values())
 
-for key in car_details.keys():
-    print(key)
-
-for value in car_details.values():
-    print(value)
-
-print(car_details.items())
-
-for key, value in car_details.items():
-    print(f"{key}: {value}")
+if "model" in car_details:
+    print("Yes, 'model' is one of the keys.")
