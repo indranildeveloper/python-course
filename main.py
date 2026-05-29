9ff0c37c4c500de446c11de4c7ba57e3aa068fa4
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-29 19:51:28 +0530

    set comprehensionk
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,56 +1,17 @@
 """
-Python Set Methods
+Set comprehension
 
-Add Items
+Set comprehension is a concise way to create sets in Python by iterating over an iterable (like a list, string, or range) and applying an optional condition.
 
-Once a set is created, you cannot change its items, but you can add new items.
-To add one item to a set use the add() method.
-
-Remove Item
-
-To remove an item in a set, use the remove(), or the discard() method.
-If the item to remove does not exist, remove() will raise an error.
-If the item to remove does not exist, discard() will NOT raise an error.
-
-You can also use the pop() method to remove an item, but this method will remove a random item, so you cannot be sure what item that gets removed.
-Sets are unordered, so when using the pop() method, you do not know which item that gets removed.
-
-The clear() method empties the set.
-
-The del keyword will delete the set completely.
-
-Copy a set
-
-We can use the copy() method to copy a set.
-
-Set Math:
-
-Set union() Method
-Return a set that contains all items from both sets, duplicates are excluded.
-
-Set intersection() Method
-Return a set that contains the items that exist in both set x, and set y.
+The basic syntax for a set comprehension is:
+{expression for item in iterable if condition}
 """
 
-fruits_set = {"apple", "orange", "banana", "cherry"}
-new_fruit_set = {"pineapple", "melon"}
-# fruits_set.add("mango")
-fruits_set.update(new_fruit_set)
-
-# fruits_set.remove("banana")
-# fruits_set.discard("abc")
-# removed_item = fruits_set.pop()
-# print(removed_item)
-
-# fruits_set.clear()
-# del fruits_set
-
-# fruits_copy = fruits_set.copy()
-# print(fruits_copy is fruits_set)
-
-x = {"apple", "banana", "cherry"}
-y = {"google", "microsoft", "apple"}
-
-# z = x.union(y)
-z = x.intersection(y)
-print(z)
+num_set = {num for num in [1, 2, 2, 2, 3]}
+square_set = {num**2 for num in range(1, 11)}
+even_nums = {num for num in range(1, 11) if num % 2 == 0}
+str_set = {char.upper() for char in "hello"}
+# print(num_set)
+# print(square_set)
+# print(even_nums)
+print(str_set)
