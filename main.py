349bb1deafa3d7485afdb5653990202a28934998
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-01 19:14:16 +0530

    arbitrary arguments
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 """
-Python function documentation
+Python *args and **kwargs
 
-Python docstrings (documentation strings) are string literals written directly below a function definition to explain what the function does.
+*args and **kwargs
+
+By default, a function must be called with the correct number of arguments.
+However, sometimes you may not know how many arguments that will be passed into your function.
+
+*args and **kwargs allow functions to accept a unknown number of arguments.
+
+Arbitrary Arguments - *args
+
+If you do not know how many arguments will be passed into your function, add a * before the parameter name.
+This way, the function will receive a tuple of arguments and can access the items accordingly.
+Arbitrary Arguments are often shortened to *args in Python documentation.
+
+What is *args?
+
+The *args parameter allows a function to accept any number of positional arguments.
+Inside the function, args becomes a tuple containing all the passed arguments.
 """
 
-# print(print.__doc__)
+
+def sum_numbers(*numbers):
+    print(numbers)
+    total = 0
+    for num in numbers:
+        total += num
+    return total
 
 
-# def divide(a, b):
-#     """Divide two numbers safely.
-
-#     Args:
-#         a (int): The dividend.
-#         b (int): The divisor.
-
-#     Returns:
-#         float: The quotient result.
-
-#     Raises:
-#         ValueError: If b equals zero.
-#     """
-#     if b == 0:
-#         raise ValueError("Division by zero.")
-#     return a / b
-
-
-def divide(a, b):
-    """
-    Divide two numbers safely.
-
-    Parameters
-    ----------
-    a : int
-        The dividend.
-    b : int
-        The divisor.
-
-    Returns
-    -------
-    float
-    """
-    return a / b
+print(sum_numbers(1, 2, 3, 4, 5))
+print(sum_numbers(10, 20))
+print(sum_numbers(5))
