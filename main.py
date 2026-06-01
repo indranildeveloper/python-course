94c1cb554ec22a0d06340c654b1a3986601dac58
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-01 12:48:18 +0530

    scope in python
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,23 +1,100 @@
 """
-Python Functions
+Python Scope
 
-Positional Arguments
+Scope
 
-When you call a function with arguments without using keywords, they are called positional arguments.
-Positional arguments must be in the correct order.
+A variable is only available from inside the region it is created. This is called scope.
 
-Keyword Arguments
+Local Scope
 
-You can send arguments with the key = value syntax.
-This way, with keyword arguments, the order of the arguments does not matter.
-The phrase Keyword Arguments is often shortened to kwargs in Python documentation.
+A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
+
+Function Inside Function
+
+The variable x is not available outside the function, but it is available for any function inside the function.
+
+Global Scope
+
+A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+Global variables are available from within any scope, global and local.
+
+Naming Variables
+
+If you operate with the same variable name inside and outside of a function, Python will treat them as two separate variables, one available in the global scope (outside the function) and one available in the local scope (inside the function).
+
+Global Keyword
+
+If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
+The global keyword makes the variable global.
+Also, use the global keyword if you want to make a change to a global variable inside a function.
+
+Nonlocal Keyword
+
+The nonlocal keyword is used to work with variables inside nested functions.
+The nonlocal keyword makes the variable belong to the outer function.
+
+The LEGB Rule
+
+Python follows the LEGB rule when looking up variable names, and searches for them in this order:
+
+    Local - Inside the current function
+    Enclosing - Inside enclosing functions (from inner to outer)
+    Global - At the top level of the module
+    Built-in - In Python's built-in namespace
 """
 
+# def my_function():
+#     x = 500
+#     print("local", x)
 
-def get_full_name(first_name="Sara", last_name="Smith"):
-    print(f"Your full name is: {first_name} {last_name}")
+#     def my_inner_func():
+#         print("inner", x)
+
+#     my_inner_func()
 
 
-get_full_name("John", "Doe")
-get_full_name(first_name="Jane", last_name="Doe")
-get_full_name()
+# my_function()
+
+# x = 500
+
+# def my_function():
+#     x = 200
+#     print("Inside function", x)
+
+
+# my_function()
+# print("Global", x)
+
+
+# def my_function():
+#     global x
+#     x = 300
+
+
+# my_function()
+# print(x)
+
+# x = 500
+
+
+# def my_function():
+#     global x
+#     x = x + 100
+#     print(x)
+
+
+# my_function()
+
+
+# def my_function():
+#     x = "John"
+
+#     def my_inner_function():
+#         nonlocal x
+#         x = "Hello"
+
+#     my_inner_function()
+#     return x
+
+
+# print(my_function())
