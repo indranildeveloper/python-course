4add8094ea692aa0c04572b47f95fa8ed98917d0
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-04 13:06:33 +0530

    abs round and sum functions
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 """
-Python len() Function
+Python abs() Function
 
-The len() function returns the number of items in an object.
+The abs() function returns the absolute value of the specified number.
 
-When the object is a string, the len() function returns the number of characters in the string.
+Python sum() Function
+
+The sum() function returns a number, the sum of all items in an iterable.
+
+Python round() Function
+
+The round() function returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
+The default number of decimals is 0, meaning that the function will return the nearest integer.
 """
 
-fruits = ["apple", "banana", "cherry"]
+# print(abs(10))
+# print(abs(-10))
+# print(abs(-10 + 5j))
 
-print(fruits.__len__())
-print("hello".__len__())
+# nums = [1, 2, 3, 4, 5]
 
-# print(len(fruits))
-# print(len("hello"))
+# print(sum(nums, 10))
+
+# print(sum(["hello", "there"], "hi"))
+
+# print(round(5.66666, 2))
+# print(round(5.1))
