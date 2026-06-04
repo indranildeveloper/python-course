a54c4d9c968cf40e2d4248c26e2b04f1ae4ceb11
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-04 18:56:38 +0530

    raise exceptions
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 """
-Python Errors
+Python raise Keyword
 
-SyntaxError -> Invalid Syntax
-IndentationError -> Wrong Indentation
-NameError -> Variables not defined
-TypeError -> Mismatch of data types
-ValueError -> Right type but inappropriate value
-IndexError -> Index not exists
-KeyError -> Key not exists
-AttributeError -> Method not exists
+The raise keyword is used to raise an exception.
+You can define what kind of error to raise, and the text to print to the user.
 """
+
+# positive_number = -1
+
+# if positive_number < 0:
+#     raise Exception("Hey you can not provide and number less than zero.")
+# else:
+#     print("Your number is all good!")
+
+number = "hello"
+
+if not type(number) is int:
+    raise TypeError("Only integers are allowed.")
+else:
+    print("Your number is all good!")
