c639f7c40d0fa29058c61fb7a870d017e5a2632b
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-06 20:33:26 +0530

    the __init__ method in class
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
 """
-Python OOP
+Python __init__() Method
 
-What is OOP?
+The __init__() Method
+All classes have a built-in method called __init__(), which is always executed when the class is being initiated.
 
-OOP stands for Object-Oriented Programming.
-
-Python is an object-oriented language, allowing you to structure your code using classes and objects for better organization and reusability.
-
-Advantages of OOP
-
-- Provides a clear structure to programs
-- Makes code easier to maintain, reuse, and debug
-- Helps keep your code DRY (Don't Repeat Yourself)
-- Allows you to build reusable applications with less code
-
-What are Classes and Objects?
-Classes and objects are the two core concepts in object-oriented programming.
-
-A class defines what an object should look like, and an object is created based on that class. For example:
-
-Class	Objects
-Fruit	Apple, Banana, Mango
-Car	    Volvo, Audi, Toyota
-
-When you create an object from a class, it inherits all the variables and functions defined inside that class.
-
-Python Classes and Objects
-
-Python Classes/Objects
-Python is an object oriented programming language.
-
-Almost everything in Python is an object, with its properties and methods.
-
-A Class is like an object constructor, or a "blueprint" for creating objects.
+The __init__() method is used to assign values to object properties, or to perform operations that are necessary when the object is being created.
 """
 
 
 class Person:
-    name = "John"
+    def __init__(self, name, age=20):
+        self.name = name
+        self.age = age
 
 
-person_one = Person()
-person_two = Person()
-person_three = Person()
-person_four = Person()
-person_five = Person()
-# del person_one
-print(person_one)
+person_one = Person("John", 22)
+person_two = Person("Jane", 24)
+
+print(person_one.name, person_one.age)
+print(person_two.name, person_two.age)
