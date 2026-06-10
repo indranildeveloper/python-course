ffff4bd850a1f7ce34138b5d1b97ca4245646e0f
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-10 19:56:25 +0530

    peroperty decorator
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
 """
 Python Encapsulation
-
-Encapsulation is about protecting data inside a class.
-
-It means keeping data (properties) and methods together in a class, while controlling how the data can be accessed from outside the class.
-
-This prevents accidental changes to your data and hides the internal details of how your class works.
-
-Why Use Encapsulation?
-
-Encapsulation provides several benefits:
-
-- Data Protection: Prevents accidental modification of data
-- Validation: You can validate data before setting it
-- Flexibility: Internal implementation can change without affecting external code
-- Control: You have full control over how data is accessed and modified
 """
 
 
@@ -24,8 +9,17 @@ class Person:
         self.__age = age  # private property
         self._salary = salary  # protected property
 
-    def get_age(self):
+    @property
+    def age(self):
         return self.__age
+
+    @age.setter
+    def age(self, new_age):
+        if self.__validate_age(new_age):
+            self.__age = new_age
+
+    # def get_age(self):
+    #     return self.__age
 
     def __validate_age(self, age):
         if age > 0:
@@ -33,12 +27,15 @@ class Person:
         else:
             raise ValueError("Age can not be less than zero.")
 
-    def set_age(self, new_age):
-        if self.__validate_age(new_age):
-            self.__age = new_age
+    # def set_age(self, new_age):
+    #     if self.__validate_age(new_age):
+    #         self.__age = new_age
 
 
 person_one = Person("John", 24, 50000)
-# person_one.age = 30
 
+# person_one.set_age(30)
+# print(person_one.get_age())
+
+person_one.age = 30
 print(person_one.age)
