131e2573799c23a80cf44613b6a51d783397e0da
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-10 18:28:41 +0530

    multiple inheritance
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
 """
-Python Inheritance
+Multiple Inheritance
 
-Inheritance allows us to define a class that inherits all the methods and properties from another class.
-
-Parent class is the class being inherited from, also called base class.
-
-Child class is the class that inherits from another class, also called derived class.
-
-Create a Child Class
-To create a class that inherits the functionality from another class, send the parent class as a parameter when creating the child class.
+Multiple inheritance in Python is a feature that allows a child class to inherit attributes and methods from more than one parent class.
 """
 
 
-class Person:
-    def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+class Flyer:
+    def __init__(self, name):
+        self.name = name
 
-    def full_name(self):
-        return f"The full name is: {self.first_name} {self.last_name}"
-
-
-class Student(Person):
-    def __init__(self, first_name, last_name, year):
-        super().__init__(first_name, last_name)
-        self.graduation_year = year
+    def fly(self):
+        return f"{self.name} flying high in the sky."
 
     def greet(self):
-        return f"Welcome, {self.first_name} {self.last_name} to the class of {self.graduation_year}."
+        return f"Hello from flyer {self.name}"
 
 
-s_one = Student("John", "Doe", 2024)
-print(s_one.graduation_year)
-print(s_one.full_name())
-print(s_one.greet())
+class Swimmer:
+    def __init__(self, name):
+        self.name = name
+
+    def swim(self):
+        return f"{self.name} is swimming good in the pond."
+
+    def greet(self):
+        return f"Hello from swimmer {self.name}"
+
+
+class Duck(Flyer, Swimmer):
+    def __init__(self, name):
+        super().__init__(name)
+
+
+captain_cook = Duck("Captain Cook")
+print(captain_cook.fly())
+print(captain_cook.swim())
+print(captain_cook.greet())
