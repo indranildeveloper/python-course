70e3f9b1d750cbeeab9dbbee378a316212148f7f
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-21 16:14:31 +0530

    generator expressions and generator methods
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,66 +1,49 @@
 """
 Python Generators
 
-Generators are iterators.
+Generator Expressions
 
-Generators are functions that can pause and resume their execution.
-
-When a generator function is called, it returns a generator object, which is an iterator.
-
-The code inside the function is not executed yet, it is only compiled. The function only executes when you iterate over the generator.
-
-Generators allow you to iterate over data without storing the entire dataset in memory.
-
-Instead of using return, generators use the yield keyword.
-
-The yield Keyword
-
-The yield keyword is what makes a function a generator.
-
-When yield is encountered, the function's state is saved, and the value is returned. The next time the generator is called, it continues from where it left off.
+Similar to list comprehensions, you can create generators using generator expressions with parentheses instead of square brackets.
 """
 
-# def count_up_to(num):
-#     count = 1
-#     while count <= num:
-#         yield count
-#         count += 1
+# def square_numbers():
+#     for num in range(5):
+#         yield num * num
 
 
-# print(count_up_to(5))
-# for num in count_up_to(5):
+# square_numbers_generator = square_numbers()
+
+# square_numbers_generator = (num * num for num in range(5))
+
+# print(square_numbers_generator)
+
+# for num in square_numbers_generator:
 #     print(num)
 
 
-# def generate_person():
-#     yield "John"
-#     yield "Jane"
-#     yield "Sara"
+# def echo_generator():
+#     while True:
+#         received = yield
+#         print("Received: ", received)
 
 
-# person_generator = generate_person()
+# generator = echo_generator()
+# next(generator)
 
-# print(next(person_generator))
-# print(next(person_generator))
-# print(next(person_generator))
-# print(next(person_generator))
-
-
-def large_sequence(number):
-    for num in range(number):
-        yield num
-        # print(num)
+# generator.send("Hello")
+# generator.send("World")
 
 
-# sequence_generator = large_sequence(1000000)
+def my_generator():
+    try:
+        yield 1
+        yield 2
+        yield 3
+    finally:
+        print("Generator closed.")
 
-# large_sequence(10000000000)
 
-for num in large_sequence(1000000):
-    print(num)
-
-# print(next(sequence_generator))
-# print(next(sequence_generator))
-# print(next(sequence_generator))
-# print(next(sequence_generator))
-# print(next(sequence_generator))
+generator = my_generator()
+print(next(generator))
+print(next(generator))
+generator.close()
