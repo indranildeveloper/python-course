10f2c2d29e3d92b248e998fa4af217fe38103656
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-22 21:30:41 +0530

    type annotation in python
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 """
-Python Delete File
+Python Type Annotations/Hints
+
+Python type annotations (also known as type hints) are an optional syntax used to explicitly specify the expected data types of variables, function parameters, and return values.
 """
 
-import os
+from typing import Union, Literal
 
-os.remove("./delete_me.txt")
+name: str = "John"
+age: int = 20
+
+scores: list[int] = [10, 20, 30]
+inventory: dict[str, int] = {"apples": 10, "bananas": 20}
+coordinates: tuple[float, float, str] = (10.0, 20.0, "banana")
+
+
+user_id: int | str = 10
+
+theme: Literal["light", "dark"] = "light"
+
+
+def greet(name: str) -> str:
+    return f"Hello, {name.upper()}"
+
+
+result = greet("John")
