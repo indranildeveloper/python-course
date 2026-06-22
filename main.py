00e67fbbe0c1c4d71af5d254c251179970a0cf87
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-22 21:36:43 +0530

    course end
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,5 @@
 """
-Python Type Annotations/Hints
+Programming With Indra
 
-Python type annotations (also known as type hints) are an optional syntax used to explicitly specify the expected data types of variables, function parameters, and return values.
+Thank You!
 """
-
-from typing import Union, Literal
-
-name: str = "John"
-age: int = 20
-
-scores: list[int] = [10, 20, 30]
-inventory: dict[str, int] = {"apples": 10, "bananas": 20}
-coordinates: tuple[float, float, str] = (10.0, 20.0, "banana")
-
-
-user_id: int | str = 10
-
-theme: Literal["light", "dark"] = "light"
-
-
-def greet(name: str) -> str:
-    return f"Hello, {name.upper()}"
-
-
-result = greet("John")
