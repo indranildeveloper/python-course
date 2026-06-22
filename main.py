c2d796acf3860b0c38f37bb5c81e7717f8e2c5f9
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-22 21:09:01 +0530

    delete a file
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
 """
-Python File Write
-
-Write to an Existing File
-
-To write to an existing file, you must add a parameter to the open() function:
-
-- "a" - Append - will append to the end of the file
-- "w" - Write - will overwrite any existing content
+Python Delete File
 """
 
-with open("demo.txt", "a") as text_file:
-    # text_file.write("Hello World.\n")
-    # text_file.write("Hello World again.\n")
-    # text_file.write("This is a new line.\n")
-    text_file.write("Hi there.")
+import os
+
+os.remove("./delete_me.txt")
